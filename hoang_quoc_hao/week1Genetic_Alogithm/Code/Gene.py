cc21505bc5e0008ed2afed4468773d7cd7e4be7c
Hoàng Quốc Hảo <32641351+haok61bkhn@users.noreply.github.com>
2018-10-19 11:20:58 +0700

    Add files via upload

---
+++ hoang_quoc_hao/week1Genetic_Alogithm/Code/Gene.py
@@ -0,0 +1,12 @@
+import random
+class Gen:
+    def __init__(self,gene):
+        self.gene=gene
+
+
+    def initializegen(self):
+        self.gene=random.uniform(-10,10)
+        
+
+            
+    
