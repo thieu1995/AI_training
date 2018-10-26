b8b94e31c267c21ae0f8645af364229861f76d25
Ha Viet Tien <viettienha98@gmail.com>
2018-10-26 09:30:27 +0700

    Add Star network and compare

--- ha_viet_tien/week2_PSO/PSO_func.py
+++ ha_viet_tien/week2_PSO/PSO_func.py
@@ -1,6 +1,8 @@
 import numpy as np
 import random
 
+
+# 4 cluster
 class PSO:
     """
     Find solution for the function x1^2 + x2^3 + x3^3 + ... + x49^2 + x50^2
@@ -16,8 +18,8 @@ class PSO:
         self.score = []
         self.fitness_score()
         self.velocity = self.generateVelocity()
-        self.Pbest = self.score
-        self.Pposition = self.pop
+        self.Pbest = np.copy(self.score)
+        self.Pposition = np.copy(self.pop)
         self.Gbest = []
         self.Gposition = []
         self.cluster = []
@@ -138,4 +140,114 @@ class PSO:
             self.update_position()
             self.fitness_score()
             min_value.append(min(self.Gbest))
+        return min_value
+
+
+
+# star
+class PSO_star:
+    """
+    Find solution for the function x1^2 + x2^3 + x3^3 + ... + x49^2 + x50^2
+    reach the min value using PSO technique with Four-cluster social network
+    """
+    def __init__(self, size_pop, size_var, constraints, \
+    #person_deg, social_deg
+    ):
+        self.size_var = size_var
+        self.constraints = constraints
+        self.size_pop = size_pop
+        self.pop = self.generateBirds()
+        self.score = []
+        self.fitness_score()
+        self.velocity = self.generateVelocity()
+        self.Pbest = np.copy(self.score)
+        self.Pposition = np.copy(self.pop)
+        self.Gbest = np.copy(self.score[0])
+        self.Gposition = np.copy(self.pop[0])
+        # self.c1 = person_deg
+        # self.c2 = social_deg
+
+
+    # generate bird swarm randomly
+    def generateBirds(self):
+        pop = []
+        for i in range(self.size_pop):
+            pop.append(np.random.uniform(self.constraints[0], self.constraints[1], size=self.size_var))
+        return pop
+
+
+    # caculate the sum of function
+    def fitness_score(self):
+        self.score[:] = []
+        for i in range(self.size_pop):
+            sum = 0
+            for j in range(self.size_var):
+                # divided by 1000 to make it smaller
+                if j % 2 == 0:
+                    sum += (self.pop[i][j]**2/1000)
+                elif j % 2 == 1:
+                    sum += (self.pop[i][j]**3/1000)
+            self.score.append(sum)
+
+
+    # generate the first velocity randomly
+    def generateVelocity(self):
+        v = []
+        for i in range(self.size_pop):
+            v.append(np.random.uniform(-2.5, 2.5, size=self.size_var))
+        return v
+
+
+    def update_Pbest(self):
+        for i in range(self.size_pop):
+            if self.score[i] < self.Pbest[i]:
+                self.Pbest[i] = np.copy(self.score[i])
+                self.Pposition[i] = np.copy(self.pop[i])
+
+    def update_Gbest(self):
+        # init global with first element
+        self.Gbest = np.copy(self.score[0])
+        self.Gposition = np.copy(self.pop[0])
+
+        # find it in the while population
+        for i in range(self.size_pop):
+            if self.score[i] < self.Gbest:
+                self.Gbest = np.copy(self.score[i])
+                self.Gposition = np.copy(self.pop[i])
+
+
+    def update_velocity(self, time, iteration, pre_w):
+        c1 = (0.5 - 2.5)*(time/iteration) + 2.5
+        c2 = (0.5 - 2.5)*(time/iteration) + 2.5
+        for i in range(self.size_pop):
+            r1 = random.random()
+            r2 = random.random()
+            # w = c1 * r1 + c2 * r2
+            w = (pre_w - 0.4)*(iteration - time)/(iteration+0.4)
+            self.velocity[i] = w * self.velocity[i] + \
+                                c1*r1*(self.Pposition[i] - self.pop[i]) + \
+                                c2*r2*(self.Gposition - self.pop[i])
+
+
+    def update_position(self):
+        for i in range(self.size_pop):
+            self.pop[i] += self.velocity[i]
+            # to make sure it in the area of interest
+            for j in range(self.size_var):
+                if self.pop[i][j] > 10 or self.pop[i][j] < -10:
+                    """
+                    self.pop[i] = (10*(np.sign(self.pop[i][j]))/self.pop[i][j])*self.pop[i]
+                    """
+                    self.pop[i][j] = 10*np.sign(self.pop[i][j])
+
+
+    def run(self, iteration):
+        min_value = []
+        for i in range(iteration):
+            self.update_Pbest()
+            self.update_Gbest()
+            self.update_velocity(i, iteration, 0.9)
+            self.update_position()
+            self.fitness_score()
+            min_value.append(self.Gbest)
         return min_value
